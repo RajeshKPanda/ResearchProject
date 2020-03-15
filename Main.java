98784373a97283d2b6b63077180b0b5e74d5a140
RajeshKPanda <rajeshpan@gmail.com>
2020-03-15 09:07:01 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,21 +3,19 @@ package academy.learnProgramming;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
+
+    private Scanner scanner = new Scanner(System.in);
+    private static MobilePhone = new MobilePhone("0039 330 1111");
 
     public static void main(String[] args) {
-        int[] myIntegers = getIntegers[5];
-        for(int i = 0; i < myIntegers.length; i++){
-            System.out.println(myIntegers[i]);
+
+        boolean quit = false;
+        startPhone();
+        printactions();
+        while(!quit){
+            
         }
-         }
 
-         public static int[] getIntegers(int number){
-             System.out.println("enter the values");
-            int[] values = new Int[number];
+    }
 
-            for(int i = 0; i < values.length; i++)
-                values[i] = scn.nextInt();
-             return values;
-         }
 }
